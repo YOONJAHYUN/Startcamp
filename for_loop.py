1721a1ce356eed0acbcde2542662bf8978bb007c
YOONJAHYUN <dbswkgus323@gmail.com>
2023-01-13 12:10:22 +0900

    start camp

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+numbers = [1,2,3,4]
+
+#numbers list가 가진 모든 요소를 순회 할때까지 
+#numbers가 가진 각 요소를 출력
+# num 이라는 변수는 정해진 이름? 아니다 -> 변수 이름 맘대로 가능
+
+for num in numbers:
+    # numbers가 가진 각 요소를 출력
+    print(num)
+print('끝났니?')
+
+numbers = {'a': 1,'b': 2}
+for num in numbers
+    print(numbers[num])
---
+++ lunch.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+menu = ['닭고기온메밀국수', '토마토 리조또', '무파마', '김치찜']
+print(menu)
+print(menu[0])
+
+# print(dir(random))
+
+print(random.choice(menu))
