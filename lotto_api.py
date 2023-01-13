0fcb4debb74f6c946dfb4f2258937236d4ec898d
YOONJAHYUN <dbswkgus323@gmail.com>
2023-01-13 14:29:14 +0900

    보너스번호 추가

--- lotto_api.py
+++ lotto_api.py
@@ -4,7 +4,7 @@
 # 회차 직접 입력
 # 입력받은 회차에 해당하는 당첨번호 확인하기 -> 6개 (보너스번호제외)
 
-# 선택사항 : 보너브 번호 확인하기
+# 선택사항 : 보너스 번호 확인하기
 
 import requests
 
@@ -13,4 +13,4 @@ print(date)
 
 r = requests.get(f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={date}').json()
 
-print(date, '회차의 당첨 번호는', r['drwtNo1'], r['drwtNo2'], r['drwtNo3'], r['drwtNo4'], r['drwtNo5'], r['drwtNo6'], '입니다.')
+print(date, '회차의 당첨 번호는', r['drwtNo1'], r['drwtNo2'], r['drwtNo3'], r['drwtNo4'], r['drwtNo5'], r['drwtNo6'], '입니다. \n그리고 보너스 번호는 ', r['bnusNo'], '입니다.')
